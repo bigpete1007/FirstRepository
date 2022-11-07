eeb6505992519456e1baea70d9b6f53e50c3d372
bigpete1007 <asiastrikes@naver.com>
2022-11-07 14:54:02 +0900

    두번째커밋

--- src/git/HelloGit.java
+++ src/git/HelloGit.java
@@ -5,6 +5,7 @@ public class HelloGit {
 	public static void main(String[] args) {
 
 		System.out.println("Hello Git Repository");
+		System.out.println("두번째작업");
 
 	}
 
--- src/git/NewFile.java
+++ src/git/NewFile.java
@@ -4,6 +4,7 @@ public class NewFile {
 
 	public static void main(String[] args) {
 		System.out.println("처음만드는 Git Repository");
+		System.out.println("두번째commit");
 	}
 
 }
